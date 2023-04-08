903589ed11de2a50917c685807c21662aa1dadff
c.winger <c.winger@oeko.de>
2023-04-08 17:44:43 +0200

    add existing schema

--- setup.py
+++ setup.py
@@ -7,9 +7,12 @@ setup(
     author="Christian Winger",
     author_email="c.winger@oeko.de",
     url="http://ek-gitlab.oeko.local/c.winger/oekolib-schema",
-    install_requires=["jsonschema", "frictionless"],
+    install_requires=["jsonschema>=4.17", "frictionless>=5.10", "requests>=2.28"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
+    package_data={
+        "oekolib.schema": ["data/**"],
+    },
 )
